30b1aa1f682ed69f1b15bd745826c3a0a54b81d0
AriyaLau <lau39@purdue.edu>
2019-02-14 16:35:36 -0500

    initialize Create_a_meeting(Actually) wasn't initialize in previous commit

---
+++ meeting_client/app/src/main/java/com/example/meetingmasterclient/Create_a_Meeting.java
@@ -0,0 +1,13 @@
+package com.example.meetingmasterclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Create_a_Meeting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_a__meeting);
+    }
+}
