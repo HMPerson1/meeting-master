956976dad4162bd4073cd3a4e07e3c1b916c421b
EJ <ewennerb@purdue.edu>
2019-02-20 12:53:34 -0500

    Added date field to event model

--- meeting_server/events/models.py
+++ meeting_server/events/models.py
@@ -1,11 +1,15 @@
 from django.db import models
-
 import uuid
+import datetime
+
+today = datetime.date.today()
+
 
 class Event(models.Model):
-    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True),
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     name = models.CharField(null=False, blank=False, required=True)
-    duration = models.DurationField(null=False, blank=False),
+    date = models.DateField(null=False, blank=False, default=today)
+    duration = models.DurationField(null=False, blank=False)
     attachment = models.FileField(default=None, required=False)
     notes = models.TextField(default=None)
 #     Todo: Add in ForeignKey stuff to link with Users
