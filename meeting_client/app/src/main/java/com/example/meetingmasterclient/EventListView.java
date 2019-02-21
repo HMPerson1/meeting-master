6195906ad1caa1df2692fa2062f58726f83b02be
Daniel <dansanchezbui@unal.edu.co>
2019-02-21 10:47:05 -0500

    Activity for event list view almost finished

--- meeting_client/app/src/main/java/com/example/meetingmasterclient/EventListView.java
+++ meeting_client/app/src/main/java/com/example/meetingmasterclient/EventListView.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -12,12 +13,16 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class EventListView extends AppCompatActivity {
+    ListView eventData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +31,8 @@ public class EventListView extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        ListView eventData = findViewById(R.id.event_data);
-        setEventsToday(eventData);
+        eventData = findViewById(R.id.event_data);
+        sendSearchRequest("");
     }
 
     @Override
@@ -40,69 +45,56 @@ public class EventListView extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         ListView eventData = findViewById(R.id.event_data);
 
+        // TODO: Input the appropriate URL into each call
         switch (item.getItemId()) {
             case R.id.this_week:
-                setEventsThisWeek(eventData);
+                sendSearchRequest("");
                 break;
             case R.id.this_month:
-                setEventsThisMonth(eventData);
+                sendSearchRequest("");
                 break;
             default:
-                setEventsToday(eventData);
+                sendSearchRequest("");
                 break;
         }
 
         return true;
     }
 
-    // TODO: Program methods to send an HTTP request to the server and parse the response
+    RequestQueue eventQueue;
 
-    private void setEventsToday(ListView eventData) {
-        /*String[][] test = {{"a1", "a1", "a1", "a1"}, {"a2", "a2", "a2", "a2"}, {"a3", "a3", "a3", "a3"}};
-        EventViewAdapter eva = new EventViewAdapter(getApplicationContext(), test);
-        eventData.setAdapter(eva);*/
+    private void sendSearchRequest(String url) {
+        eventQueue = Volley.newRequestQueue(this);
 
-        JSONObject params = new JSONObject();
-        // TODO: Add parameters to JSON
-
-
-        // TODO: Insert URL in the method
-        sendSearchRequest("", params);
-    }
-
-    private void setEventsThisWeek(ListView eventData) {
-        JSONObject params = new JSONObject();
-        // TODO: Add parameters to JSON
-
-
-        // TODO: Insert URL in the method
-        sendSearchRequest("", params);
-    }
-
-    private void setEventsThisMonth(ListView eventData) {
-        JSONObject params = new JSONObject();
-        // TODO: Add parameters to JSON
-
-
-        // TODO: Insert URL in the method
-        sendSearchRequest("", params);
-    }
-
-    private void sendSearchRequest(String url, JSONObject params) {
-        JsonObjectRequest eventRequest = new JsonObjectRequest(Request.Method.GET, url, params, new Response.Listener<JSONObject>() {
+        JsonArrayRequest eventRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
             @Override
-            public void onResponse(JSONObject response) {
-                // TODO: Parse JSON response and display information on ListView
+            public void onResponse(JSONArray response) {
+                String[][] eventInfo = new String[response.length()][4];
 
+                try {
+                    for (int i = 0; i < response.length(); i++) {
+                        eventInfo[i][0] = response.getJSONObject(i).getString("admin");
+                        eventInfo[i][1] = response.getJSONObject(i).getString("name");
+                        eventInfo[i][2] = response.getJSONObject(i).getString("date");
+                        eventInfo[i][3] = response.getJSONObject(i).getString("location");
+                    }
+
+                    EventViewAdapter results = new EventViewAdapter(getApplicationContext(), eventInfo);
+                    eventData.setAdapter(results);
+                    eventData.setVisibility(View.VISIBLE);
+                } catch(JSONException e) {
+                    Toast.makeText(getApplicationContext(), "An error has occurred while processing the information", Toast.LENGTH_SHORT).show();
+                } finally {
+                    eventQueue.stop();
+                }
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Toast.makeText(getApplicationContext(), "An error has occurred", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "An error has occurred in the network", Toast.LENGTH_SHORT).show();
             }
         });
 
-        // TODO: Define request queue and add request to the queue
-
+        eventQueue.add(eventRequest);
     }
 }
