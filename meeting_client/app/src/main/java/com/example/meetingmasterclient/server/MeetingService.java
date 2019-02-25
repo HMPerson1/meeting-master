6f76589afe71378afe44f6a840b5a3d2e679711f
HMPerson1 <hmperson1@gmail.com>
2019-02-25 00:43:37 -0500

    Add retrofit for more endpoints

--- meeting_client/app/src/main/java/com/example/meetingmasterclient/server/MeetingService.java
+++ meeting_client/app/src/main/java/com/example/meetingmasterclient/server/MeetingService.java
@@ -9,17 +9,71 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Query;
 
 public interface MeetingService {
+    /**
+     * may fail with {@link RegistrationError}
+     */
     @POST("/register/")
     Call<AuthToken> register(@Body RegistrationData data);
 
+    /**
+     * may fail with "Unable to log in with provided credentials."
+     */
     @POST("/rest-auth/login/")
     Call<AuthToken> login(@Body LoginData data);
 
+    /**
+     * needs authentication <br>
+     * never fails (if authenticated)
+     */
+    @POST("/rest-auth/logout/")
+    Call<Void> logout();
+
+    /**
+     * needs authentication <br>
+     * never fails (if authenticated)
+     */
+    @GET("/rest-auth/user/")
+    Call<UserProfile> getCurrentUser();
+
+    /**
+     * needs authentication <br>
+     * data.pk is ignored <br>
+     * may fail with {@link UserProfileError}
+     */
+    @PUT("/rest-auth/user/")
+    Call<UserProfile> putCurrentUser(@Body UserProfile data);
+
+    /**
+     * may fail with {@link ResetPasswordError}
+     */
+    @POST("/rest-auth/password/reset/")
+    Call<Void> resetPassword(@Body ResetPasswordData data);
+
+    /**
+     * may fail with {@link ConfirmResetPasswordError}
+     */
+    @POST("/rest-auth/password/reset/confirm/")
+    Call<Void> confirmResetPassword(@Body ConfirmResetPasswordData data);
+
+    /**
+     * needs authentication <br>
+     * never fails (if authenticated)
+     */
     @GET("/users/")
     Call<List<UserProfile>> users(@Query("search") String search);
+
+
+    /* ******************** *
+     * Dumb data containers *
+     * ******************** */
+
+    class AuthToken {
+        public String key;
+    }
 
     class RegistrationData {
         public String username;
@@ -89,12 +143,32 @@ public interface MeetingService {
                     ", profile_picture='" + profile_picture + '\'' +
                     '}';
         }
-
-
     }
 
-    class AuthToken {
-        public String key;
+    class UserProfileError {
+        public String[] pk;
+        public String[] username;
+        public String[] first_name;
+        public String[] last_name;
+        public String[] email;
+        public String[] phone_number;
+        public String[] profile_picture;
+        public String[] non_field_errors;
+
+        @Override
+        @NonNull
+        public String toString() {
+            return "UserProfileError{" +
+                    "pk=" + Arrays.toString(pk) +
+                    ", username=" + Arrays.toString(username) +
+                    ", first_name=" + Arrays.toString(first_name) +
+                    ", last_name=" + Arrays.toString(last_name) +
+                    ", email=" + Arrays.toString(email) +
+                    ", phone_number=" + Arrays.toString(phone_number) +
+                    ", profile_picture=" + Arrays.toString(profile_picture) +
+                    ", non_field_errors=" + Arrays.toString(non_field_errors) +
+                    '}';
+        }
     }
 
     class LoginData {
@@ -104,6 +178,62 @@ public interface MeetingService {
         public LoginData(String username, String password) {
             this.username = username;
             this.password = password;
+        }
+    }
+
+    class ResetPasswordData {
+        public String email;
+
+        public ResetPasswordData(String email) {
+            this.email = email;
+        }
+    }
+
+    class ResetPasswordError {
+        public String[] email;
+        public String[] non_field_errors;
+
+        @Override
+        @NonNull
+        public String toString() {
+            return "ResetPasswordError{" +
+                    "email=" + Arrays.toString(email) +
+                    ", non_field_errors=" + Arrays.toString(non_field_errors) +
+                    '}';
+        }
+    }
+
+    class ConfirmResetPasswordData {
+        public String new_password1;
+        public String new_password2;
+        public String uid;
+        public String token;
+
+        public ConfirmResetPasswordData(String new_password1, String new_password2, String uid, String token) {
+            this.new_password1 = new_password1;
+            this.new_password2 = new_password2;
+            this.uid = uid;
+            this.token = token;
+        }
+    }
+
+    class ConfirmResetPasswordError {
+        public String[] new_password1;
+        public String[] new_password2;
+        public String[] uid;
+        public String[] token;
+        public String[] non_field_errors;
+
+        @Override
+        @NonNull
+        public String toString() {
+            return "ConfirmResetPasswordError{" +
+                    "new_password1=" + Arrays.toString(new_password1) +
+                    ", new_password2=" + Arrays.toString(new_password2) +
+                    ", uid=" + Arrays.toString(uid) +
+                    ", token=" + Arrays.toString(token) +
+                    ", non_field_errors=" + Arrays.toString(non_field_errors) +
+                    '}';
         }
     }
 }
