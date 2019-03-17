29241bb73bab2d127f562857131cf660fbfd1793
Daniel <dansanchezbui@unal.edu.co>
2019-03-17 11:40:13 -0500

    Minor changes to event list view

--- meeting_client/app/src/main/java/com/example/meetingmasterclient/EventViewAdapter.java
+++ meeting_client/app/src/main/java/com/example/meetingmasterclient/EventViewAdapter.java
@@ -7,24 +7,26 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
+import com.example.meetingmasterclient.server.MeetingService;
+
 // TODO: Set up adapter to store entire event object
 public class EventViewAdapter extends BaseAdapter {
     Context context;
-    String[][] eventInfo;
+    List<MeetingService.EventsData> eventInfo;
 
-    public EventViewAdapter(Context context, String[][] eventInfo) {
+    public EventViewAdapter(Context context, List<MeetingService.EventsData> eventInfo) {
         this.context = context;
         this.eventInfo = eventInfo;
     }
 
     @Override
     public int getCount() {
-        return eventInfo.length;
+        return eventInfo.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return eventInfo[position];
+        return eventInfo.get(position);
     }
 
     @Override
@@ -38,16 +40,18 @@ public class EventViewAdapter extends BaseAdapter {
         View infl = inflater.inflate(R.layout.event_view_item, null);
 
         TextView from = (TextView)infl.findViewById(R.id.from);
-        from.setText(eventInfo[position][0]);
+        // TODO: Make query to obtain name of admin
+        from.setText(eventInfo.get(position).event_admin);
 
         TextView name = (TextView)infl.findViewById(R.id.event_name);
-        name.setText(eventInfo[position][1]);
+        name.setText(eventInfo.get(position).event_name);
 
         TextView date = (TextView)infl.findViewById(R.id.event_date);
-        date.setText(eventInfo[position][2]);
+        date.setText(eventInfo.get(position).event_date);
 
         TextView place = (TextView)infl.findViewById(R.id.event_place);
-        place.setText(eventInfo[position][3]);
+        // TODO: Make query to obtain location details
+        place.setText(eventInfo.get(position).event_location);
 
         return infl;
     }
