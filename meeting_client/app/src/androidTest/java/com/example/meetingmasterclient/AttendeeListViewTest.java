c30111d4e8cf46b8ca2edf1d8ab4317a1476c4e9
Daniel <dansanchezbui@unal.edu.co>
2019-03-20 12:00:37 -0500

    Started coding UI unit tests

---
+++ meeting_client/app/src/androidTest/java/com/example/meetingmasterclient/AttendeeListViewTest.java
@@ -0,0 +1,42 @@
+package com.example.meetingmasterclient;
+
+import android.content.Intent;
+
+import com.example.meetingmasterclient.server.MeetingService;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.junit.Rule;
+import org.junit.Test;
+
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.matcher.BoundedMatcher;
+import androidx.test.rule.ActivityTestRule;
+
+public class AttendeeListViewTest {
+    private static Matcher<MeetingService.UserProfile> attendeeIsEqual(final MeetingService.UserProfile profile) {
+        return new BoundedMatcher<MeetingService.UserProfile, MeetingService.UserProfile>(MeetingService.UserProfile.class) {
+            @Override
+            public boolean matchesSafely(MeetingService.UserProfile up) {
+                // TODO: Insert equality condition for remaining attributes
+                return up.pk == profile.pk && up.username.equals(profile.username) && up.first_name.equals(profile.first_name);
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("attendee is equal");
+            }
+        };
+    }
+
+    //@Rule
+    //public ActivityTestRule<AttendeeListView> activityRule = new ActivityTestRule<>(AttendeeListView.class, false, false);
+
+    @Test
+    public void testAttendeeList() {
+        // Assuming the event's id is passed in the intent
+        Intent intent = new Intent();
+        intent.putExtra("id", 1);
+        //activityRule.launchActivity(intent);
+    }
+}
