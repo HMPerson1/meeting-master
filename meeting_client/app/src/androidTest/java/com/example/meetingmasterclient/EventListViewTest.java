3ec52f7931591c6ee1caf7e1d65c697bbf62eacd
Daniel <dansanchezbui@unal.edu.co>
2019-03-20 16:14:23 -0500

    Worked on tests for event list view

--- meeting_client/app/src/androidTest/java/com/example/meetingmasterclient/EventListViewTest.java
+++ meeting_client/app/src/androidTest/java/com/example/meetingmasterclient/EventListViewTest.java
@@ -5,7 +5,14 @@ import android.content.Intent;
 import org.junit.Rule;
 import org.junit.Test;
 
+import androidx.test.espresso.DataInteraction;
+import androidx.test.espresso.Espresso;
 import androidx.test.rule.ActivityTestRule;
+
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.anything;
 
 public class EventListViewTest {
     @Rule
@@ -22,6 +29,20 @@ public class EventListViewTest {
     public void eventListViewFirstTest() {
         activityRule.launchActivity(createEventListViewIntent(1));
 
+        DataInteraction data = Espresso.onData(anything()).inAdapterView(withId(R.id.event_data)).atPosition(0);
+
+        data.onChildView(withId(R.id.from)).check(matches(withText("masternasu")));
+        data.onChildView(withId(R.id.event_name)).check(matches(withText("Autographs From Nasu")));
+        data.onChildView(withId(R.id.event_date)).check(matches(withText("2019-03-30")));
+        // TODO: Check location
+        //data.onChildView(withId(R.id.event_place)).check(matches(withText("2019-03-30")));
+    }
+
+    @Test
+    public void eventListViewSecondTest() {
+        activityRule.launchActivity(createEventListViewIntent(2));
+
 
     }
 }
+
