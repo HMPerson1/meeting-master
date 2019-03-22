5fcfe4b6062e4e9a23a1262fd547384a2206cbc7
HMPerson1 <hmperson1@gmail.com>
2019-03-22 16:26:55 -0400

    Fix current user invitations endpoint

--- meeting_server/api/invitations/tests.py
+++ meeting_server/api/invitations/tests.py
@@ -65,3 +65,35 @@ class InvitationsByEventTests(TestCase):
             {'user_id': self.charlie.pk, 'event_id': self.alice_party.pk, 'status': Invitation.ACCEPTED,
              'edit_permission': False},
         ]
+
+
+class CurrentUserInvitationsTest(TestCase):
+    def setUp(self):
+        super(CurrentUserInvitationsTest, self).setUp()
+        self.alice = UserProfile.objects.create(django_user=auth_models.User.objects.create(
+            username='alice', first_name='Alice', last_name='Q.', email='alice@example.com'))
+        self.bob = UserProfile.objects.create(django_user=auth_models.User.objects.create(
+            username='bob', first_name='Bob', last_name='Q.', email='bob@example.com'))
+
+        self.party_place = Location.objects.create(street_address='123 Main St.', city='Anywhere', state='IN')
+        self.bob_party = Event.objects.create(
+            event_admin=self.bob,
+            event_name='Party!',
+            event_date=datetime.date.today() + datetime.timedelta(days=3),
+            event_time=datetime.time(16, 10),
+            event_duration=datetime.time(1, 0),
+            event_location=self.party_place,
+            notes='PREPARE TO PARTY',
+        )
+        Invitation.objects.create(user_id=self.alice, event_id=self.bob_party, status=Invitation.ACCEPTED)
+
+        self.client = APIClient()
+        self.client.credentials(
+            HTTP_AUTHORIZATION='Token ' + create_token(TokenModel, self.alice.django_user, None).key)
+
+    def testGetInvitations(self):
+        response = self.client.get("/invitations/user-invitations")
+        assert response.status_code == status.HTTP_200_OK
+        assert json.loads(response.content) == \
+               [{'user_id': self.alice.pk, 'event_id': self.bob_party.pk, 'status': Invitation.ACCEPTED,
+                 'edit_permission': False}]
