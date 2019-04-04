a027275c11f6320fc706b3634ffe259bd6e04295
HMPerson1 <hmperson1@gmail.com>
2019-04-04 18:31:20 -0400

    Add LocationUpdateService
    TODO: start/stop this service with the correct buttons in EventDetails
    TODO: correct URL for posting location

--- meeting_client/app/src/main/java/com/example/meetingmasterclient/utils/Notifications.java
+++ meeting_client/app/src/main/java/com/example/meetingmasterclient/utils/Notifications.java
@@ -4,49 +4,62 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.os.Build;
+import android.support.annotation.IntDef;
+import android.support.annotation.RequiresApi;
 
 import com.example.meetingmasterclient.R;
+
+import java.util.Arrays;
 
 public final class Notifications {
     public static final String CHANNEL_INVITE_ID = "invite";
     public static final String CHANNEL_EDIT_ID = "edit";
     public static final String CHANNEL_ARRIVED_HOME_ID = "arrived_home";
+    public static final String CHANNEL_LIVE_LOCATION_ID = "live_location";
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private static NotificationChannel mkNotificationChannel(String id, CharSequence name, @Importance int importance, String description) {
+        NotificationChannel ret = new NotificationChannel(id, name, importance);
+        ret.setDescription(description);
+        return ret;
+    }
 
     public static void ensureNotificationChannels(Context context) {
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is new and not in the support library
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
-
-            // invites
-            if (notificationManager.getNotificationChannel(CHANNEL_INVITE_ID) == null) {
-                NotificationChannel inviteChannel = new NotificationChannel(
-                        CHANNEL_INVITE_ID,
-                        context.getString(R.string.channel_invite_name),
-                        NotificationManager.IMPORTANCE_DEFAULT);
-                inviteChannel.setDescription(context.getString(R.string.channel_invite_description));
-                notificationManager.createNotificationChannel(inviteChannel);
-            }
-
-            // edits
-            if (notificationManager.getNotificationChannel(CHANNEL_EDIT_ID) == null) {
-                NotificationChannel editChannel = new NotificationChannel(
-                        CHANNEL_EDIT_ID,
-                        context.getString(R.string.channel_edit_name),
-                        NotificationManager.IMPORTANCE_DEFAULT);
-                editChannel.setDescription(context.getString(R.string.channel_edit_description));
-                notificationManager.createNotificationChannel(editChannel);
-            }
-
-            // arrived home
-            if (notificationManager.getNotificationChannel(CHANNEL_ARRIVED_HOME_ID) == null) {
-                NotificationChannel arrivedHomeChannel = new NotificationChannel(
-                        CHANNEL_ARRIVED_HOME_ID,
-                        context.getString(R.string.channel_arrived_home_name),
-                        NotificationManager.IMPORTANCE_DEFAULT);
-                arrivedHomeChannel.setDescription(context.getString(R.string.channel_edit_description));
-                notificationManager.createNotificationChannel(arrivedHomeChannel);
-            }
+            NotificationChannel[] channels = new NotificationChannel[]{
+                    // invites
+                    mkNotificationChannel(
+                            CHANNEL_INVITE_ID,
+                            context.getString(R.string.channel_invite_name),
+                            NotificationManager.IMPORTANCE_DEFAULT,
+                            context.getString(R.string.channel_invite_description)),
+                    // edits
+                    mkNotificationChannel(
+                            CHANNEL_EDIT_ID,
+                            context.getString(R.string.channel_edit_name),
+                            NotificationManager.IMPORTANCE_DEFAULT,
+                            context.getString(R.string.channel_edit_description)),
+                    // arrived home
+                    mkNotificationChannel(
+                            CHANNEL_ARRIVED_HOME_ID,
+                            context.getString(R.string.channel_arrived_home_name),
+                            NotificationManager.IMPORTANCE_DEFAULT,
+                            context.getString(R.string.channel_arrived_home_description)),
+                    // live location
+                    mkNotificationChannel(
+                            CHANNEL_LIVE_LOCATION_ID,
+                            context.getString(R.string.channel_live_location_name),
+                            NotificationManager.IMPORTANCE_LOW,
+                            context.getString(R.string.channel_live_location_description))
+            };
+            context.getSystemService(NotificationManager.class)
+                    .createNotificationChannels(Arrays.asList(channels));
         }
+    }
+
+    @IntDef({NotificationManager.IMPORTANCE_DEFAULT, NotificationManager.IMPORTANCE_HIGH, NotificationManager.IMPORTANCE_LOW, NotificationManager.IMPORTANCE_MIN, NotificationManager.IMPORTANCE_NONE, NotificationManager.IMPORTANCE_UNSPECIFIED})
+    private @interface Importance {
     }
 }
