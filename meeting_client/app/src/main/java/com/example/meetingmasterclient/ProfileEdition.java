13be9d96567bd11c6058eff610e6356886d52987
Aaron Lynn <lynn13@purdue.edu>
2019-04-09 01:32:24 -0400

    Figured out how to autofill a field in ProfileEdition

--- meeting_client/app/src/main/java/com/example/meetingmasterclient/ProfileEdition.java
+++ meeting_client/app/src/main/java/com/example/meetingmasterclient/ProfileEdition.java
@@ -3,6 +3,7 @@ package com.example.meetingmasterclient;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -10,7 +11,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class ProfileEdition extends AppCompatActivity {
-    private TextInputLayout textInputFirstName;
+    private TextInputEditText textInputFirstName;
     private TextInputLayout textInputLastName;
     private TextInputLayout textInputUsername;
     private TextInputLayout textInputEmailAddress;
@@ -23,11 +24,13 @@ public class ProfileEdition extends AppCompatActivity {
         setContentView(R.layout.activity_profile_edition);
 
         //initialize user fields
-        textInputFirstName = findViewById(R.id.text_edit_first_name);
+        textInputFirstName = findViewById(R.id.thisisatest//text_edit_first_name);
         textInputLastName = findViewById(R.id.text_edit_last_name);
         textInputUsername = findViewById(R.id.text_edit_username);
         textInputEmailAddress = findViewById(R.id.text_edit_email);
         textInputPhoneNumber = findViewById(R.id.text_edit_phone_number);
+        String name = "name";
+        textInputFirstName.setText(name);
 
         confirmButton = findViewById(R.id.confirm_profile_changes_button);
         confirmButton.setOnClickListener(new View.OnClickListener() {
