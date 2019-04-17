eba2c72293255a388297c99b333dd984a3a43fab
EJ <ewennerb@purdue.edu>
2019-04-17 12:57:44 -0400

    Added endpoint for getting invitee locations

--- meeting_server/api/invitations/urls.py
+++ meeting_server/api/invitations/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'invitee-locations/(?P<event_id>.+)$', views.InvitationEventView.as_view(), name='api-invitation-location-by-event'),
     url(r'event-invitations/(?P<event_id>.+)$', views.InvitationEventView.as_view(), name='api-invitations-by-event'),
     url(r'user-invitations$', views.InvitationUserView.as_view(), name='api-invitations-by-user'),
     url(r'(?P<event_id>.+)/(?P<user_id>.+)/update_status$', views.InvitationStatusChangeView.as_view(),
